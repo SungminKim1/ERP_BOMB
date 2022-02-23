b8288d618fcc76c73d64b38b6f3a1cbf7f544c43
SungMin Kim <87789944+SungminKim1@users.noreply.github.com>
2022-02-23 18:42:38 +0900

    Update VoteController.java

--- src/main/java/com/greedy/erp_bomb/vote/controller/VoteController.java
+++ src/main/java/com/greedy/erp_bomb/vote/controller/VoteController.java
@@ -40,7 +40,7 @@ public class VoteController {
 		List<VoteDTO> regVoteList = new ArrayList<>();
 		
 		/* 진행중, 종료 부분을 위한 코딩 */
-		for (VoteDTO voteDTO : voteList) {						// 투표의 종료일과 오늘 날짜를 비교
+		for (VoteDTO voteDTO : voteList) {							// 투표의 종료일과 오늘 날짜를 비교
 			if (voteDTO.getEndDate().before(date)) {			
 				endVoteList.add(voteDTO);
 			}
@@ -50,9 +50,9 @@ public class VoteController {
 		}
 		
 		mv.addObject("tab", tab);								// 전체, 진행중, 종료를 구분하기위한 값(HTML에서 표시를 다르게해줌)
-		mv.addObject("voteList", voteList);						// 전체 게시글
-		mv.addObject("endVoteList", endVoteList);				// 종료된 게시글
-		mv.addObject("regVoteList", regVoteList);				// 진행중인 게시글
+		mv.addObject("voteList", voteList);							// 전체 게시글
+		mv.addObject("endVoteList", endVoteList);						// 종료된 게시글
+		mv.addObject("regVoteList", regVoteList);						// 진행중인 게시글
 		
 		mv.setViewName("/vote/vote");
 		
@@ -62,15 +62,15 @@ public class VoteController {
 	@PostMapping("insertVote")									// 신규 투표
 	public ModelAndView insertVote(ModelAndView mv, @RequestParam String title, @RequestParam String insertMember,
 			@RequestParam java.sql.Date endDate, @RequestParam String content
-			, @AuthenticationPrincipal UserImpl user) {			// 투표 작성시 받는 매개변수 각 변수의 클래스가 달라 @ModelAttribute 사용 안함
+			, @AuthenticationPrincipal UserImpl user) {					// 투표 작성시 받는 매개변수 각 변수의 클래스가 달라 @ModelAttribute 사용 안함
 		
 		/* 작성일 */
 		java.sql.Date date = new java.sql.Date(System.currentTimeMillis());
 		
 		MemberDTO member = new MemberDTO();
-		member.setName(user.getName());							// 현재 로그인한 직원의 이름
+		member.setName(user.getName());								// 현재 로그인한 직원의 이름
 		
-		VoteDTO vote = new VoteDTO();							// 투표 작성에 필요한 값 넣기
+		VoteDTO vote = new VoteDTO();								// 투표 작성에 필요한 값 넣기
 		vote.setMember(member);									// 로그인한 직원
 		vote.setTitle(title);									// 투표 제목
 		vote.setRegDate(date);									// 작성일
@@ -78,16 +78,16 @@ public class VoteController {
 		vote.setContent(content);								// 내용
 		
 		/* 신규 투표 작성시 후보가 있나없나 판별 */
-		if(!insertMember.isEmpty()) {							// 신규 투표 작성시 후보를 등록하면서 작성
+		if(!insertMember.isEmpty()) {								// 신규 투표 작성시 후보를 등록하면서 작성
 			VoteOptionDTO voteOption = new VoteOptionDTO();
 			
 			voteOption.setVote(vote);							// 투표 정보
-			voteOption.setMember(member);						// 후보 등록하는 계정
-			voteOption.setDesc(insertMember);					// 후보
+			voteOption.setMember(member);							// 후보 등록하는 계정
+			voteOption.setDesc(insertMember);						// 후보
 			
 			voteService.insertVote(voteOption);
 		} else {
-			voteService.insertVote(vote);						// 후보 등록을 하지 않고 투표 게시글만 작성
+			voteService.insertVote(vote);							// 후보 등록을 하지 않고 투표 게시글만 작성
 		}
 		
 		mv.setViewName("redirect:/vote/vote");
@@ -103,7 +103,7 @@ public class VoteController {
 		int[] voteEqual = new int[voteDetail.getVoteOptionList().size()];			// 해당 게시판의 후보의 수
 		int max = 0;
 		
-		for(int i = 0; i < voteEqual.length; i++) {									// 최다 득표 후보를 판별하기 위한 반복문
+		for(int i = 0; i < voteEqual.length; i++) {						// 최다 득표 후보를 판별하기 위한 반복문
 			voteEqual[i] = voteDetail.getVoteOptionList().get(i).getVoteCount();
 			if(max < voteEqual[i]){
                 max = voteEqual[i];
@@ -123,7 +123,7 @@ public class VoteController {
 			vote.setMember(mem);
 			
 			if(max == vote.getVoteCount()) {
-				vote.setTopVote(1);													// 최다 득표한 후보
+				vote.setTopVote(1);							// 최다 득표한 후보
 			}
 		}
 		
@@ -189,18 +189,18 @@ public class VoteController {
 	@PostMapping(value = "plusCandi", produces = "application/json; charset=UTF-8")			
 	@ResponseBody
 	public void plusCandidate(@RequestParam String candiInsert, @RequestParam int votenum,
-			@AuthenticationPrincipal UserImpl user) {						// 후보 추가
+			@AuthenticationPrincipal UserImpl user) {					// 후보 추가
 		
 		MemberDTO member = new MemberDTO();
-		member.setName(user.getName());										// 후보를 추가하려는 계정의 이름
+		member.setName(user.getName());								// 후보를 추가하려는 계정의 이름
 		
 		VoteDTO vote = new VoteDTO();
-		vote.setSerialNo(votenum);											// 추가할 투표 게시판의 번호
+		vote.setSerialNo(votenum);								// 추가할 투표 게시판의 번호
 		
 		VoteOptionDTO voteOption = new VoteOptionDTO();
 		voteOption.setMember(member);
 		voteOption.setVote(vote);
-		voteOption.setDesc(candiInsert);									// 후보의 이름
+		voteOption.setDesc(candiInsert);							// 후보의 이름
 		
 		voteService.insertCandidate(voteOption);
 	}
